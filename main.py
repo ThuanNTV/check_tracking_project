85e9e3f40a9bd7832b2def4bc4c9e18cdd5f0080
ThuanNTV <thuanntv721@gmail.com>
2025-05-07 02:00:12 +0700

    add file requirements.txt

--- main.py
+++ main.py
@@ -240,4 +240,5 @@ if __name__ == '__main__':
     if not os.path.exists('templates'):
         os.makedirs('templates')
 
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
